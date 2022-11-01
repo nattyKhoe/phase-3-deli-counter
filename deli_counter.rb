80937809f2b1aa04be465b83763c5666d9790528
Natasha <natasha.khoe@gmail.com>
2022-11-01 20:26:34 +1100

    commit tree b8fe7e891a38bdcb357c13352ea9556bc40624b7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line (array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else 
+        new_array = array.each_with_index.map {|element, index| "#{index+1}. #{element}"}
+        puts "The line is currently: #{new_array.join(" ")}"
+    end
+end
+
+def take_a_number (array, customer)
+    array << customer
+    puts "Welcome, #{customer}. You are number #{array.find_index(customer)+1} in line."
+end
+
+def now_serving (array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
+
+
+
+
